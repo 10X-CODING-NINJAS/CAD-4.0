4f2d319a8d0a659805cf569573b26056b035d3dc
pratyushsrivastava1sgn <pratyushsrivastava1sgn@gmail.com>
2026-03-28 22:30:24 +0530

    dates change

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -49,32 +49,32 @@ const Timeline = () => {
 
       <div className="node n1">
         <div className="card">
-          <h3>INDUCTION STARTS</h3>
-          <p>6 April 10AM</p>
+          <h3>Registration opens</h3>
+          <p>23 March</p>
         </div>
         <div className="dot"></div>
       </div>
 
       <div className="node n2">
         <div className="card">
-          <h3>HACKATHON STARTS</h3>
-          <p>6 April 11:30AM</p>
+          <h3>PPT submission</h3>
+          <p>1 April</p>
         </div>
         <div className="dot"></div>
       </div>
 
       <div className="node n3">
         <div className="card">
-          <h3>FINAL REVIEW RESULT</h3>
-          <p>7 April 2PM</p>
+          <h3>Shortlisted team announced</h3>
+          <p>4 April</p>
         </div>
         <div className="dot"></div>
       </div>
 
       <div className="node n4">
         <div className="card">
-          <h3>CLOSING CEREMONY</h3>
-          <p>7 April 3PM</p>
+          <h3>Offline Hackathon</h3>
+          <p>6 April 8AM</p>
         </div>
         <div className="dot"></div>
       </div>
